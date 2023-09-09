92e2a8e82620856b08a73c590aefeb8b85d0f14b
KauaNilton <kaua4nilton@gmail.com>
2023-09-09 16:33:00 -0300

    Resolvendo exercicios com stream api

---
+++ src/exercicios/Exercicio03.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio03 {
+    // Verificando se todos os números da lista são positivos.
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean numerosPositivos = numeros.stream()
+                .allMatch(n -> n >= 0);
+        System.out.println("Todos os números da lista são postivos? " + numerosPositivos);
+    }
+}
